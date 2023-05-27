1d2dfb410d494815d4058c4e6bb175c261bb7b70
XsChen524 <cxs52hku@connect.hku.hk>
2023-05-28 02:50:53 +0800

    Add sequelize migrations

--- database/migrations/20230527134425-init-meta.js
+++ database/migrations/20230527134425-init-meta.js
@@ -1,22 +1,43 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("meta", {
+			id: {
+				primaryKey: true,
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				comment: "responsibility id",
+			},
+			name: {
+				type: Sequelize.DataTypes.STRING(100),
+				allowNull: false,
+				comment: "responsibility name",
+			},
+			description: {
+				type: Sequelize.DataTypes.STRING(200),
+				allowNull: true,
+				defaultValue: null,
+				comment: "responsibility description",
+			},
+			projectId: {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				comment: "id of project the responsibility belongs to",
+			},
+			previous: {
+				// Need to convert to type number[] in service
+				type: Sequelize.DataTypes.TEXT,
+				allowNull: false,
+				defaultValue: "[]",
+				comment: "previous txn ids of the responsibility",
+			},
+		});
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+	async down(queryInterface) {
+		await queryInterface.dropTable("meta");
+	},
 };
--- database/migrations/20230527134440-init-txns.js
+++ database/migrations/20230527134440-init-txns.js
@@ -1,22 +1,59 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("txns", {
+			id: {
+				// Digest length 66 for Ethereum, need to confirm HF
+				primaryKey: true,
+				type: Sequelize.DataTypes.STRING(66),
+				allowNull: false,
+				comment: "PK_Transaction digest",
+			},
+			blockId: {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				comment: "Block id containing txn",
+			},
+			type: {
+				type: Sequelize.DataTypes.STRING,
+				allowNull: false,
+				comment: "operation type",
+			},
+			operatorId: {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				comment: "User id who commits the txn",
+			},
+			description: {
+				type: Sequelize.DataTypes.STRING(200),
+				allowNull: true,
+				defaultValue: null,
+				comment: "responsibility description",
+			},
+			fromVendor: {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				comment: "id from whom responsibility transferred",
+			},
+			toVendor: {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				comment: "id to whom responsibility transferred",
+			},
+			createAt: {
+				type: Sequelize.DataTypes.DATE,
+				allowNull: false,
+			},
+			committedAt: {
+				type: Sequelize.DataTypes.DATE,
+				allowNull: true,
+			},
+		});
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+	async down(queryInterface) {
+		await queryInterface.dropTable("txns");
+	},
 };
