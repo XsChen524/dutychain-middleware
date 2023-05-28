511c0040b24b8846a2d702a2f11ad3b2b18e7e33
XsChen524 <cxs52hku@connect.hku.hk>
2023-05-29 01:28:09 +0800

    Utils to convert string to number[]

---
+++ app/utils/utils.js
@@ -0,0 +1,14 @@
+function stringToNumberArray(str) {
+	const tmpArray = str.split(",");
+	for (let i = 0; i < tmpArray.length; i += 1) {
+		tmpArray[i] = tmpArray[i].replace("[", "");
+		tmpArray[i] = tmpArray[i].replace("]", "");
+	}
+	const returnArray = [];
+	for (let i = 0; i < tmpArray.length; i += 1) {
+		returnArray.push(Number(tmpArray[i]));
+	}
+	return returnArray;
+}
+
+module.exports = { stringToNumberArray };
