da73d567f488ec6ecd687be00cc4f37ea26b0432
XsChen524 <cxs52hku@connect.hku.hk>
2023-06-12 19:59:20 +0800

    mockdata

---
+++ examples/mockdata.js
@@ -0,0 +1,16 @@
+/* eslint-disable array-bracket-spacing */
+
+const organizations = [
+	{ id: 1, name: "CheungKong" },
+	{ id: 2, name: "NewWorld" },
+	{ id: 3, name: "House730" },
+	{ id: 4, name: "PwC" },
+	{ id: 5, name: "Midland" },
+];
+
+const channels = [
+	{ projectId: 1, name: "Pokfulam Garden", vendorId: [1, 3, 4] },
+	{ projectId: 2, name: "New City", vendorId: [2, 3, 4] },
+];
+
+module.exports = { organizations, channels };
