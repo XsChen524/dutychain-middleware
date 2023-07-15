694b4015b681f09d1c3e7098610780d2f6ca5c8f
David Mak <davidlammak@gmail.com>
2023-07-15 08:46:30 -0700

    Add citation
    Add citation

--- app/blockchain/CAUtil.js
+++ app/blockchain/CAUtil.js
@@ -4,6 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/*
+# *****************************************************************************
+# -------------------------------Referencce------------------------------------
+# This script is the demo script of Hyperledger Fabric network and we do some
+# modifications on the script so that it suits better in our project.
+
+# Reference: https://hyperledger-fabric.readthedocs.io/en/release-2.5/
+# *****************************************************************************
+*/
+
 'use strict';
 
 const adminUserId = 'admin';
