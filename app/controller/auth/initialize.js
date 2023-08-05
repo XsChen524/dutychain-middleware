c399c2e26ea5cebfcfc41b040c64e6b9bcaeba71
ZimuSama <3229329616@qq.com>
2023-08-05 16:52:35 +0800

    Fix: Fix the administrative accounts registration.

--- app/controller/auth/initialize.js
+++ app/controller/auth/initialize.js
@@ -10,15 +10,30 @@ class InitializeController extends Controller {
 		let success = true;
 		if (isFirstCalled) {
 			for (const [key, value] of Object.entries(unencryptedPasswords)) {
-				const admin = await ctx.service.auth.administration.register({
+				console.log(key, value); // Output default administrator passwords, just for debug!!!
+				let previousId = (await this.ctx.model.Auth.User.find().sort({ id: -1 }))[0];
+				previousId = previousId === undefined ? 0 : previousId.id;
+				const hashPassword = await this.ctx.genHash(value);
+				const admin = await this.ctx.model.Auth.User.create({
+					id: previousId + 1,
 					name: "admin" + key,
-					password: value,
+					password: hashPassword,
 					email: "admin@org" + key + ".hk",
 					organization: key,
-					role: "vendor",
 					isAdmin: true,
-					wallet: "admin",
+					role: "vendor",
+					walletId: "admin",
 				});
+
+				// const admin = await ctx.service.auth.administration.register({
+				// 	name: "admin" + key,
+				// 	password: value,
+				// 	email: "admin@org" + key + ".hk",
+				// 	organization: key,
+				// 	role: "vendor",
+				// 	isAdmin: true,
+				// 	wallet: "admin",
+				// });
 
 				success = success && !!admin;
 			}
